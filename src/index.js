46c040e2b8912bc628eba07c92750733779b4612
duncanhunter <duncan@duncanhunter.com.au>
2025-02-01 15:49:04 +1100

    init

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import Database from 'better-sqlite3';
+
+const app = new Hono()
+const db = new Database('./db/database.sqlite');
+
+app.get('/', async (c) => {
+  const todos = await db.prepare("SELECT * FROM todos").all();
+
+  return c.html(`
+    <!DOCTYPE html>
+      <html lang="en">
+          <head>
+              <meta name="viewport" content="width=device-width">
+              <meta name="description" content="htmx todos">
+              <title>Pluralsight HTMX Foundation</title>
+          </head>
+          <body>
+            <form method="post" action="/">
+              <input name="name" placeholder="New todo" required autocomplete="off">
+              <button type="submit">Add</button>
+            </form>
+            <ul>
+              ${todos.map(todo => `<li>${todo.name} <a href="/delete/${todo.id}">delete</a></li>`).join('')}
+            </ul>
+          </body>
+      <html>
+  `);
+})
+
+app.post("/", async (c) => {
+  const { name } = await c.req.parseBody();
+  await db.prepare("INSERT INTO todos (name) VALUES (?)").bind(name).run();
+
+  return c.redirect("/");
+});
+
+app.get("/delete/:id", async (c) => {
+  const id = c.req.param("id");
+  await db.prepare("DELETE FROM todos WHERE id = ?").bind(id).run();
+
+  return c.redirect("/");
+});
+
+const port = 3000
+console.log(`Server is running on http://localhost:${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
