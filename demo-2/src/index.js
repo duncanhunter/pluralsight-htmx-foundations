2ed7c2dbfbe95b729c69f9aba8abcef69331fca4
duncanhunter <duncan@duncanhunter.com.au>
2025-02-16 14:01:03 +1100

    oob stategy

--- demo-2/src/index.js
+++ demo-2/src/index.js
@@ -2,7 +2,7 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import Database from 'better-sqlite3';
 
-const app = new Hono()
+const app = new Hono();
 const db = new Database('./db/database.sqlite');
 
 app.get('/', async (c) => {
@@ -15,55 +15,83 @@ app.get('/', async (c) => {
               <meta name="viewport" content="width=device-width">
               <meta name="description" content="htmx todos">
               <title>Pluralsight HTMX Foundation</title>
-              <script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous"></script>    
+              <script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous"></script>
+              <style>
+                .deleting, .loading-form {
+                  display: none;
+                }
+                .htmx-request.deleting,
+                .htmx-request .deleting,
+                .htmx-request.loading-form,
+                .htmx-request .loading-form  {
+                  display: inline
+                }
+              </style>
           </head>
           <body>
             <form
-              hx-post="/"
+              hx-indicator=".loading-form"
               hx-target="next ul"
               hx-swap="afterbegin"
-            >
-              <input name="name" placeholder="New todo" required autocomplete="off">
-              <button type="submit">Add</button>
+              hx-post="/">
+                <input name="name" placeholder="New todo" required autocomplete="off">
+                <button type="submit">Add</button>
             </form>
+            <div id="error"></div>
             <ul>
-              ${todos.map(todo => `
-                <li>${todo.name}
-                  <button
-                    hx-delete="/${todo.id}"
-                    hx-swap="outerHTML"
-                    hx-target="closest li">
-                      delete
-                  </button>
-                </li>
+                ${todos.map(todo => `
+                  <li>${todo.name}
+                    <button
+                      hx-indicator=".deleting"
+                      hx-target="closest li"
+                      hx-swap="outerHTML"
+                      hx-delete="/${todo.id}">
+                        delete
+                    </button>
+                  </li>
                 `).join('')}
+                <div class="deleting">deleting...</div>
+                <div class="loading-form">loading form...</div>
             </ul>
+            <div><span id="todo-count">${todos.length}</span> items left</div>
           </body>
       <html>
   `);
-})
+});
 
 app.post("/", async (c) => {
   const { name } = await c.req.parseBody();
+  const existingTodo = await db.prepare("SELECT * FROM todos WHERE name = ?").get(name);
+
+  if(existingTodo) {
+    return c.html(`
+      <div id="error" style="color:red;" hx-swap-oob="true">Todo already exists</div>
+    `)
+  }
+
   const { lastInsertRowId } = await db.prepare("INSERT INTO todos (name) VALUES (?)").bind(name).run();
+  const todos = await db.prepare("SELECT * FROM todos").all();
 
   return c.html(`
+    <span id="todo-count" hx-swap-oob="true">${todos.length}</span>
+    <div id="error" style="color:red;" hx-swap-oob="true"></div>
     <li>${name}
       <button
-        hx-delete="/${lastInsertRowId}"
+        hx-target="closest li"
         hx-swap="outerHTML"
-        hx-target="closest li">
+        hx-delete="/${lastInsertRowId}">
           delete
       </button>
     </li>
-  `);
+    `);
 });
 
 app.delete("/:id", async (c) => {
   const id = c.req.param("id");
   await db.prepare("DELETE FROM todos WHERE id = ?").bind(id).run();
+  const todos = await db.prepare("SELECT * FROM todos").all();
 
-  return c.body(null);
+  return c.html(`<span id="todo-count" hx-swap-oob="true">${todos.length}</span>`);
 });
 
 serve({
