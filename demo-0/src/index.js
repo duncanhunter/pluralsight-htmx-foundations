b5b6ad1625536ca102831745857d59e10797b2a2
duncanhunter <duncan@duncanhunter.com.au>
2025-03-02 20:29:28 +1100

    revert demo 0

--- demo-0/src/index.js
+++ demo-0/src/index.js
@@ -15,39 +15,18 @@ app.get('/', async (c) => {
               <meta name="viewport" content="width=device-width">
               <meta name="description" content="htmx todos">
               <title>Pluralsight HTMX Foundation</title>
-              <script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous"></script>
-              <style>
-                .deleting, .loading {
-                  display: none;
-                }
-                .htmx-request.deleting,
-                .htmx-request.loading {
-                  display: inline;
-                }
-              </style>
             </head>
           <body>
-            <form 
-              hx-indicator=".loading"
-              hx-swap="afterBegin"
-              hx-target="next ul"
-              hx-post="/">
+            <form method="post" action="/">
               <input name="name" placeholder="New todo" required autocomplete="off">
               <button type="submit">Add</button>
             </form>
             <ul>
                 ${todos.map(todo => `
                   <li>${todo.name}
-                    <button 
-                      hx-indicator=".deleting"
-                      hx-target="closest li"
-                      hx-swap="outerHTML"
-                      hx-delete="/${todo.id}">
-                        delete</button>
+                    <a href="/${todo.id}">delete</a>
                   </li>
                 `).join('')}
-                <div class="deleting">deleting...</div>
-                <div class="loading">loading...</div>
              </ul>
             <div><span id="todo-count">${todos.length}</span> items left</div>
           </body>
@@ -59,20 +38,14 @@ app.post("/", async (c) => {
   const { name } = await c.req.parseBody();
   const { lastInsertRowid } = await db.prepare("INSERT INTO todos (name) VALUES (?)").bind(name).run();
 
-  return c.html(`
-    <li>${name}
-      <button 
-        hx-target="closest li"
-        hx-swap="outerHTML"
-        hx-delete="/${lastInsertRowid}">delete</button>
-    </li>`);
+  return c.redirect("/");
 });
 
-app.delete("/:id", async (c) => {
+app.get("/delete/:id", async (c) => {
   const id = c.req.param("id");
   await db.prepare("DELETE FROM todos WHERE id = ?").bind(id).run();
 
-  return c.body(null);
+  return c.redirect("/");
 });
 
 serve({
