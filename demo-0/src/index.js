f6de52880ad5f0888f194e2f86287f7299c7981c
duncanhunter <duncan@duncanhunter.com.au>
2025-03-26 07:40:37 +1100

    fix typo

--- demo-0/src/index.js
+++ demo-0/src/index.js
@@ -6,8 +6,7 @@ const app = new Hono();
 const db = new Database('./db/database.sqlite');
 
 app.get('/', async (c) => {
-  const todos = await db.prepare(" SELECT * FROM todos ORDER BY id DESC").all();
-
+  const todos = await db.prepare("SELECT * FROM todos ORDER BY id DESC").all();
   return c.html(`
     <!DOCTYPE html>
       <html lang="en">
